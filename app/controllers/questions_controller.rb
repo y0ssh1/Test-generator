495785d2d586b0d4ccc1949a6391b1d152994c83
yosshi0774 <yosshi0774@gmail.com>
2016-06-17 01:54:51 +0900

    add grading function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -69,10 +69,9 @@ class QuestionsController < ApplicationController
 
   # GET /questions/result
   def result
-    hash = Question.find(session[:question]).map{ |model| [model.id, model] }.to_h #該当する３つの文とIDを見つける（順番は適当）
-    @targets = session[:question].map { |id| hash[id] } #問題文の順番にmodelを並び変える．
-    @responses = @targets.map.with_index { |v, i| params[i.to_s] } #テキストボックスの値を受け取る
-
+    hash = Question.find(session[:question]).map{ |model| [model.id, model] }.to_h # 該当する３つの文とIDを見つける（順番は適当）
+    @targets = session[:question].map { |id| hash[id] } # 問題文の順番にmodelを並び変える．
+    @responses = @targets.map.with_index { |v, i| params[i.to_s] } # テキストボックスの値を受け取る
   end
 
   private
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,4 +3,10 @@ module QuestionsHelper
     return text if text.nil?
     h(text).gsub(/\r\n|\r|\n/, "<br>").html_safe
   end
+
+  def count_match(a, b)
+    [a, b].map(&:flatten).transpose.count do |pair|
+      pair[0] == pair[1]
+    end
+  end
 end
