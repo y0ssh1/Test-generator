efdefcc9c48ff15ae1c78ee0179b6ac54e2e636e
yosshi0774 <yosshi0774@gmail.com>
2016-06-20 23:55:28 +0900

    add footer and header

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :questions do
     collection do
-      get 'test(/:size)' => "questions#test"
+      get 'test/(:question_set_id/size(/:size))' => "questions#test"
       post 'result'
     end
   end
