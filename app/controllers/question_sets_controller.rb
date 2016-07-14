78aa48f10a3534d240ad948d4270ed9af2b15d9d
Hiroaki KARASAWA <cubicroonn@gmail.com>
2016-07-15 02:26:13 +0900

    not create only new

--- app/controllers/question_sets_controller.rb
+++ app/controllers/question_sets_controller.rb
@@ -1,19 +1,34 @@
 class QuestionSetsController < ApplicationController
+  before_action :set_question_set, only: [:show, :edit, :update, :destroy]
+
   def show
-    @question_set = QuestionSet.find(params[:id])
   end
 
   def edit
-    @question_set = QuestionSet.find(params[:id])
   end
 
   def new
-    @question_set = QuestionSet.create(user: current_user)
-    redirect_to action: 'edit', id: @question_set.id
+    @question_set = QuestionSet.new(user: current_user)
+    # redirect_to action: 'edit', id: @question_set.id
+  end
+
+  def create
+    @question_set = QuestionSet.new(title: params[:title], comment: params[:comment], user: current_user)
+
+    (params[:texts] || []).each do |text|
+      body = text.gsub(/<(.*?)>/, '_____')
+      answer = text.scan(/<(.*?)>/).flatten.join(',')
+      @question_set.questions << Question.create(body: body, answer: answer)
+    end
+
+    if @question_set.save
+      redirect_to @question_set
+    else
+      render :new
+    end
   end
 
   def update
-    @question_set = QuestionSet.find(params[:id])
     @question_set.assign_attributes(title: params[:title], comment: params[:comment])
     @question_set.destroy_all_questions!
 
@@ -23,13 +38,13 @@ class QuestionSetsController < ApplicationController
       @question_set.questions << Question.create(body: body, answer: answer)
     end
 
-    @question_set.save!
+    @question_set.save
 
-    redirect_to action: 'show', id: params[:id]
+    redirect_to @question_set
   end
 
   def destroy
-    QuestionSet.find(params[:id]).destroy
+    @question_set.destroy
 
     redirect_to action: 'index'
   end
@@ -55,5 +70,11 @@ class QuestionSetsController < ApplicationController
     hash = Question.find(session[:question]).map{ |model| [model.id, model] }.to_h # 該当する３つの文とIDを見つける（順番は適当）
     @targets = session[:question].map { |id| hash[id] } # 問題文の順番にmodelを並び変える
     @responses = @targets.map.with_index { |v, i| params[i.to_s] || [] } # テキストボックスの値を受け取る
+  end
+
+  private
+
+  def set_question_set
+    @question_set = QuestionSet.find(params[:id])
   end
 end
